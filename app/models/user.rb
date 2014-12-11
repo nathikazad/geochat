f62c57183d0439eaf3cfbc56d5dac65442534ba9
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-11 01:25:54 -0800

    fix syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :chat_rooms, -> { uniq }
 
-  validates_presence_of :fb_name, fb_id:
+  validates_presence_of :fb_name, :fb_id
   validates_uniqueness_of :fb_id
 
   def self.find_by_facebook_id(fb_id)
