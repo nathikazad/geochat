018a71b35635f11abdd32623513f15bbb371b820
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-11 00:06:29 -0800

    config rails g to generate rspec on controller generations

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Geochat
   class Application < Rails::Application
+    config.generators do |g|
+        g.rest_frameworl :rspec, :fixture => false, :views => false
+        g.integration_tool :rspec, :fixture => false, :views => false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
