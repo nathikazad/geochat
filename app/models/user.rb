f3f299fe6441ed19f0ce63b5be10671f307a426a
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-11 01:14:21 -0800

    add basic user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :chat_rooms, -> { uniq }
+
+  validates_presence_of :fb_name, fb_id:
+  validates_uniqueness_of :fb_id
+
   def self.find_by_facebook_id(fb_id)
     User.find_or_create_by(fb_id:fb_id)
   end
