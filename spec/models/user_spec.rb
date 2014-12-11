cd55f8a72aa749b17e854c7f36e45bde0e8c5a18
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-11 01:44:59 -0800

    create test that tests factory girl data creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe User do
+  it "has a valid factory" do
+    expect(build(:user)).to be_valid
+  end
+
   it "is valid with nickname, fb_id, and fb_name"
   it "is invalid without a fb_name"
   it "is invalid without a fb_id"
