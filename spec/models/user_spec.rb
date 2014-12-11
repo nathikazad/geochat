a7d3b3e3fad10fe4d4d13cdfba37a0645f3ae95c
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-11 02:28:26 -0800

    create the first four tests
    is valid with a nickname, fb_id, and fb_name
    is invalid without a fb_name
    is invalid without a fb_id
    is invalid with a duplicate fb_id

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,29 @@ describe User do
     expect(build(:user)).to be_valid
   end
 
-  it "is valid with nickname, fb_id, and fb_name"
-  it "is invalid without a fb_name"
-  it "is invalid without a fb_id"
-  it "is invalid with a duplicate facebook id"
+  it "is valid with nickname, fb_id, and fb_name" do
+    expect(build(:user, nick_name: "imuzz")).to be_valid
+  end
+
+  it "is invalid without a fb_name" do
+    user = build(:user, fb_name: nil)
+    user.valid?
+    expect(user.errors[:fb_name]).to include("can't be blank") 
+  end
+
+  it "is invalid without a fb_id" do
+    user = build(:user, fb_id: nil)
+    user.valid?
+    expect(user.errors[:fb_id]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate facebook id" do
+    art = create(:user)
+    nathik = build(:user, fb_id: art.fb_id)
+    nathik.valid?
+    expect(nathik.errors[:fb_id]).to include("has already been taken")
+  end
+  
   it "returns a new contact if you search by non-existent facebook id"
   it "returns a pre-existing contact when you search by existent facebook id"
 end
