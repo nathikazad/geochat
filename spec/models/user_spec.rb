f06fb6f7671815933f4e2646669e3955301bb383
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-11 17:44:08 -0800

    create tests for User model class method #find_by_facebook_id

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,7 +27,13 @@ describe User do
     nathik.valid?
     expect(nathik.errors[:fb_id]).to include("has already been taken")
   end
-  
-  it "returns a new contact if you search by non-existent facebook id"
-  it "returns a pre-existing contact when you search by existent facebook id"
+
+  it "returns a new contact if you search by non-existent facebook id" do
+    expect { User.find_by_facebook_id(Faker::Internet.password(64)) }.to change(User, :count).by(1)
+  end
+
+  it "returns a pre-existing contact when you search by existent facebook id" do
+    user = create(:user)
+    expect(User.find_by_facebook_id(user.fb_id)).to eq(user)
+  end
 end
