10d5e0d911a6abbb19162395caeeab7d93d0a8fe
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-15 21:34:02 -0800

    create first Api controller test that works with doorkeeper

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-describe "Users API", type: :request do
-  it ""
+describe Api::V1::UsersController, type: :request do
+  let!(:application) { Doorkeeper::Application.create!(:name => "MyApp", :redirect_uri => "http://app.com") }
+  let!(:user) { create(:user) }
+  let!(:token) { Doorkeeper::AccessToken.create! :application_id => application.id, :resource_owner_id => user.id }
+
+  it "updates the users nickname with the specified parameter" do
+    patch "/api/v1/user", user: attributes_for(:user, nick_name: "new name"), :access_token => token.token
+    user.reload
+    expect("new name").to eq(user.nick_name)
+  end
 end
