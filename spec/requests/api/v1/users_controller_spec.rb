8af4c684f492bc872b9e3c0811a44cd2adc4e626
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-15 22:16:27 -0800

    refactor PATCH #update with context block

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -5,9 +5,18 @@ describe Api::V1::UsersController, type: :request do
   let!(:user) { create(:user) }
   let!(:token) { Doorkeeper::AccessToken.create! :application_id => application.id, :resource_owner_id => user.id }
 
-  it "updates the users nickname with the specified parameter" do
-    patch "/api/v1/user", user: attributes_for(:user, nick_name: "new name"), :access_token => token.token
-    user.reload
-    expect("new name").to eq(user.nick_name)
+  describe "PATCH #update" do
+    update = "/api/v1/user"
+    
+    context "with valid attributes" do
+      it "updates the users nickname with the specified parameter" do
+        patch update, user: attributes_for(:user, nick_name: "new name"), :access_token => token.token
+        user.reload
+        expect(user.nick_name).to eq("new name")
+      end  
+    end
+
+    # context "with invalid attributes" do
+    # end
   end
 end
