8d62ac9f4c43cfd6743356d28af4d38e03c0c261
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-15 22:31:10 -0800

    add GET #show tests for users__controller

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -5,18 +5,42 @@ describe Api::V1::UsersController, type: :request do
   let!(:user) { create(:user) }
   let!(:token) { Doorkeeper::AccessToken.create! :application_id => application.id, :resource_owner_id => user.id }
 
+  describe "GET #show" do
+    let(:url) { "/api/v1/user" }
+    
+    context "with valid attributes" do
+      it "returns the current user" do
+        get url, user: attributes_for(:user), access_token: token.token
+        expect(response.code).to eq("200")
+        json = JSON.parse(response.body)
+        expect(json['fb_name']).to eq user.fb_name
+        expect(json['id']).to eq user.id
+      end
+    end
+
+    context "with invalid attributes" do
+      it "returns invalid response code" do
+        get url
+        expect(response.code).to eq("401")
+      end
+    end
+  end
+
   describe "PATCH #update" do
-    update = "/api/v1/user"
+    let(:url) { "/api/v1/user" }
     
     context "with valid attributes" do
       it "updates the users nickname with the specified parameter" do
-        patch update, user: attributes_for(:user, nick_name: "new name"), :access_token => token.token
+        patch url, user: attributes_for(:user, nick_name: "new name"), :access_token => token.token
         user.reload
         expect(user.nick_name).to eq("new name")
       end  
     end
-
-    # context "with invalid attributes" do
-    # end
+    context "with invalid attributes" do
+      it "returns invalid response code" do  
+        patch url
+        expect(response.code).to eq("401")
+      end
+    end
   end
 end
