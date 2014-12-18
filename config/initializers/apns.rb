30864132ea6d50de71e7297b5e1161ca9d3aa364
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-18 03:24:48 -0800

    rails app works with apns locally

---
+++ config/initializers/apns.rb
@@ -0,0 +1,9 @@
+  APNS.host = 'gateway.sandbox.push.apple.com'
+  APNS.pem = File.join(Rails.root, "CombinedCertKey.pem")
+  APNS.port = 2195
+  APNS.pass = "AAPLintheTree23"
+
+  device_token = "ba1d3340473f244737a56456bc72b9cc8ae027328e26fa158ce5e6b8f7ef26bf"
+  # APNS.send_notification(device_token, "Fuck you bro")
+
+  binding.pry
