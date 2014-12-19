dae3158ebcff9c8f38a4682db11ce8b3f58b5564
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-18 21:01:16 -0800

    create call back method taht notifies the chat room when message gets created

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,11 +3,17 @@ class Message < ActiveRecord::Base
   belongs_to :chat_room
   before_save :add_index
 
+  after_create :notify_chat_room
+
   def add_index
     if self.chat_room.messages.count!=0
       self.index_number=self.chat_room.messages.last(2)[0].index_number+1
     else
       self.index_number=0
     end
+  end
+
+  def notify_chat_room
+    ChatRoom.send_notifications(self.chat_room_id)
   end
 end
