c336fc6225210002e9080ffbc8fd6e81648a1ef8
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-25 12:52:20 -0800

    add presence validations for content, user_id, and chat_room_id

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,8 @@ class Message < ActiveRecord::Base
   belongs_to :chat_room
   before_save :add_index
 
+  validates_presence_of :content, :user_id, :chat_room_id
+
   after_create :notify_chat_room
 
   def add_index
